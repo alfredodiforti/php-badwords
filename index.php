d2aede89f905f3fac4b31ec2db9b631a93e2cfa3
Alfredo <adf.palermo@icloud.com>
2020-12-14 17:38:24 +0100

    Step One

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+<?php
+$testo = 'test di prova inserimento testo'
+
+?>
+
+<p>il seguente testo è inserito tramite php: <?php echo $testo;  ?> </p>
+   
+</body>
+</html>
