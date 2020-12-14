fdc50f5a4dae7ff6328641b4910901982fbe5caa
Alfredo <adf.palermo@icloud.com>
2020-12-14 20:35:49 +0100

    esercizio completo

--- index.php
+++ index.php
@@ -8,11 +8,19 @@
 <body>
 
 <?php
-$testo = 'test di prova inserimento testo'
-
+$testo = 'È arrivato: M1 è il primo chip Apple creato su misura per il Mac, e offre prestazioni incredibili, tecnologie dedicate e un’efficienza energetica straordinaria. In più, è stato pensato fin dall’inizio per avere un’intesa perfetta con macOS Big Sur, il sistema operativo desktop più evoluto al mondo. Grazie a livelli mai visti di performance per watt, il Mac con chip M1 rappresenta l’inizio di una nuova era. Non è un aggiornamento. È una vera e propria rivoluzione.'
+?>
+<?php 
+$badword = $_GET['badword'];
 ?>
 
-<p>il seguente testo è inserito tramite php: <?php echo $testo;  ?> </p>
+<?php $testobarrato = str_replace($badword,'***',$testo) ;?>
+<p>il seguente testo è inserito tramite php: <?php echo $testobarrato;  ?> </p>
+<?php echo strlen($testo);
+?>
+
+
+
    
 </body>
 </html>
